f5bd20ec62c9fa867af9fc3a353e694ee1e86bfe
Jorge Salmerón <jsalmeron18@outlook.com>
2022-04-13 16:33:59 -0500

    Example 3. Object with multi properties added

---
+++ weekly_mission_2/examples0/example0_3.js
@@ -0,0 +1,19 @@
+//Ejemplo 3. Objeto con diferentes propiedades
+
+const myObject3 = {
+    name: "Homero Simpson",
+    age: 55,
+    nickname: [
+     "Mr Simpson",
+     "Don barredora"
+    ],
+    address: {
+      zip_code: 55512,
+      street: "Av. Siempre viva #123",
+      city: "CDMX"	    
+    }
+}
+console.log("Ejemplo 3. Objeto con diferentes propiedades")
+console.log(myObject3)
+console.log(myObject3.name)
+console.log(myObject3["address"])
