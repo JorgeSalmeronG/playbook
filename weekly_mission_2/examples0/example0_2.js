ddb3a67cac60c8145d52eb01863e6fea5fb7a414
Jorge Salmerón <jsalmeron18@outlook.com>
2022-04-13 16:19:49 -0500

    Example 2, Object with properties added

---
+++ weekly_mission_2/examples0/example0_2.js
@@ -0,0 +1,9 @@
+//Ejemplo 2. Creación de objeto con propiedades
+
+const myObject2 = {
+    name: "Jorge",
+    last_name: "Salmerón",
+    height: 1.76
+}
+console.log("Ejemplo 2: Objeto con propiedades")
+console.log(myObject2)
