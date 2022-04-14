621c466b2e296d224c72d789742fba971344ea8e
Jorge Salmerón <jsalmeron18@outlook.com>
2022-04-14 15:06:24 -0500

    Example 15: Sorting list with -sort-

---
+++ weekly_mission_2/examples1/example1_15.js
@@ -0,0 +1,6 @@
+//Uso de -sort- para ordenar.
+
+const things = ['milk', 'coffee', 'sugar', 'honey', 'apple', 'carrot']
+console.log("Ejemplo 15: Elementos ordenados usando -sort-")
+console.log("before: "+ things)
+console.log("after: " + things.sort())
