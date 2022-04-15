b955b8ddaf0494d0bd84f56167d24ebcd3da361f
Jorge Salmerón <jsalmeron18@outlook.com>
2022-04-15 15:36:41 -0500

    Adding exercise1_2.js

---
+++ weekly_mission_2/exercises/1/exercise1_2.js
@@ -0,0 +1,11 @@
+//Modelado Twitter
+
+const user = {
+	username: "ActualidadRT",
+	web_page: "actualidad.rt.com",
+	joined: "December 2009",
+	following : 67,
+	followers: 3531223,
+
+}
+console.log(`The user @${user.username} joined on ${user.joined} and has ${user.followers} followers`)
