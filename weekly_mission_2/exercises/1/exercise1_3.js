5cc76a80416d955a9078db42e0a546af1bd1bca9
Jorge Salmerón <jsalmeron18@outlook.com>
2022-04-15 16:11:06 -0500

    Adding exercise1_3.js

---
+++ weekly_mission_2/exercises/1/exercise1_3.js
@@ -0,0 +1,12 @@
+//Modelado Uber
+
+
+const user = {
+	username: "Jorge Salmerón",
+	phone: "+52 9876 5432",
+	email: "jsalme@msn.mx",
+	profile: 4.7,
+	travels: 50,
+	
+}
+console.log(`Usuario: ${user.username} realizó ${user.travels} viajes`)
