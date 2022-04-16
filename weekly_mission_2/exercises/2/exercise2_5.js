89824cf73b932ed553bc46b0b1c7df11a08f50db
Jorge Salmerón <jsalmeron18@outlook.com>
2022-04-15 22:16:28 -0500

    Ejercise 5. Finding with -find-

---
+++ weekly_mission_2/exercises/2/exercise2_5.js
@@ -0,0 +1,67 @@
+const explorers = [
+ {
+   name: "Explorer 1",
+   exercises_completed: 10,
+   rate: 99,
+   city: "CDMX",
+   stack: [
+     "js",
+     "c#"
+   ],
+   missions: {
+     onboarding: {
+       isFinished: true,
+       exercisesFinished: true
+     },
+     frontend: {
+       isFinished: true,
+       exercisesFinished: true
+     }
+   }
+ },
+ {
+   name: "Explorer 2",
+   exercises_completed: 9,
+   city: "Veracruz",
+   rate: 50,
+   stack: [
+     "js"
+   ],
+   missions: {
+     onboarding: {
+       isFinished: false,
+       exercisesFinished: false
+     },
+     frontend: {
+       isFinished: false,
+       exercisesFinished: false
+     }
+   }
+ },
+ {
+   name: "Explorer 3",
+   exercises_completed: 3,
+   city: "Sonora",
+   rate: 100,
+   stack: [
+     "elixir"
+   ],
+   missions: {
+     onboarding: {
+       isFinished: true,
+       exercisesFinished: true
+     },
+     frontend: {
+       isFinished: false,
+       exercisesFinished: false
+     }
+   }
+ }
+]
+
+//Ejercicio 5. Busca el 1er explorer con city CDMX usando Find
+
+const explorer_cdmx = explorers.find((item) => item.city === "CDMX")
+console.log("Exercise 5. Find things with -find-")
+console.log(explorer_cdmx)
+
