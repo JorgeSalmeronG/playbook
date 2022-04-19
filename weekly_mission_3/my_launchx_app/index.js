8869133a2b2110f4e5d761c7911b97084211ecd0
Jorge Salmerón <jsalmeron18@outlook.com>
2022-04-19 18:28:23 -0500

    Adding -index.js- file

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,9 @@
+const MissionCommander = require('./app/missionCommander')
+const carlo = new MissionCommander("Carlo")
+console.log(carlo.name)
+
+const rodrigo = new MissionCommander("Rodrigo")
+console.log(rodrigo.name)
+
+const fer = new MissionCommander("Fernanda")
+console.log(fer.name)
