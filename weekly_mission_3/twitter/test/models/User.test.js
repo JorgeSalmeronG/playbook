1c1daf34c4284035961fbf7f69fdaac5ec0d436b
Jorge Salmerón <jsalmeron18@outlook.com>
2022-04-21 10:34:38 -0500

    Se agrega código para test #2

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -12,4 +12,12 @@ describe("Unit Tests for User class", () => {
 		expect(user.dateCreated).not.toBeUndefined()
 		expect(user.lastUpdated).not.toBeUndefined()
 	});
+
+	test('2)Add getters', () => {
+		const user= new User(1,"carlogilmar","carlo","bio")
+		expect(user.getUsername).toBe("carlogilmar")
+		expect(user.getBio).toBe("bio")
+		expect(user.getDateCreated).not.toBeUndefined()
+		expect(user.getLastUpdated).not.toBeUndefined()
+	})
 })
