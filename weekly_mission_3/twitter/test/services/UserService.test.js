cd3bc32bdb0dfa25921e89ada90ed2c0b738cd53
Jorge Salmerón <jsalmeron18@outlook.com>
2022-04-22 22:20:53 -0500

    4.2) Se Modifican archivos para satisfacer Requerimiento 2

--- weekly_mission_3/twitter/test/services/UserService.test.js
+++ weekly_mission_3/twitter/test/services/UserService.test.js
@@ -9,5 +9,16 @@ describe("Test for UserService", () => {
 		expect(user.id).toBe(1)
 		expect(user.bio).not.toBeUndefined()
 	})
+
+	test("2). Get all user data in a list", () => {
+		const user = UserService.create(1, "jorgesalme","jorge")
+		const userInfoList = UserService.getInfo(user)
+		expect(userInfoList[0]).toBe(1)
+		expect(userInfoList[1]).toBe("jorgesalme")
+		expect(userInfoList[2]).toBe("jorge")
+		expect(userInfoList[3]).toBe("Sin bio")
+	})
+
+	
 })
 
