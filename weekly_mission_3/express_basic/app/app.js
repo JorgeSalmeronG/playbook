d15c3a871efccdf388f1ec193fd53216fbaabe38
Jorge Salmerón <jsalmeron18@outlook.com>
2022-04-25 17:44:03 -0500

    6) Se agrega nueva ruta /explorers/:name, que indica recibirá un parametro

--- weekly_mission_3/express_basic/app/app.js
+++ weekly_mission_3/express_basic/app/app.js
@@ -16,10 +16,18 @@ app.get('/launchx', (req,res) => {
 	res.send('Bienvenidos a launchx')
 })
 
+//Ruta que regresa un objeto
 app.get('/explorersInNode', (req,res) => {
 	const explorer = {name : "Jorge Salme", msg : "Hello"}
 	res.send(explorer)
 })
+
+//Ruta que recibe un query params
+
+app.get('/explorers/:explorer',(req,res) => {
+	res.send(req.params)
+})
+
 
 app.listen(port, () => {
 	console.log(`example app listening on port ${port}`)
