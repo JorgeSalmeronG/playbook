f4c0a679840b0b9b50243467c507b35dc0bf4fa6
Jorge Salmerón <jsalmeron18@outlook.com>
2022-04-25 17:11:10 -0500

    5) Se agrega nueva ruta /explorersInNode que regresa un objeto

--- weekly_mission_3/express_basic/app/app.js
+++ weekly_mission_3/express_basic/app/app.js
@@ -16,6 +16,11 @@ app.get('/launchx', (req,res) => {
 	res.send('Bienvenidos a launchx')
 })
 
+app.get('/explorersInNode', (req,res) => {
+	const explorer = {name : "Jorge Salme", msg : "Hello"}
+	res.send(explorer)
+})
+
 app.listen(port, () => {
 	console.log(`example app listening on port ${port}`)
 })
