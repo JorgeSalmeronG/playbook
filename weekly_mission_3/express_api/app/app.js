81029b5e792f43d31ede42ff55bcef9dbc0e8352
Jorge Salmerón <jsalmeron18@outlook.com>
2022-04-25 18:43:07 -0500

    4) Se Agrega GET que te regresa un explorer mediante un ID

--- weekly_mission_3/express_api/app/app.js
+++ weekly_mission_3/express_api/app/app.js
@@ -8,13 +8,23 @@ const port= 3000
 // HTTP METHODS
 app.get('/v1/explorers', (req,res) => {
 	console.log(`API explorers GET ALL requests ${new Date()}`)
-	const explorer1 = {id: 1, name: "Charly"}
+	const explorer1 = {id: 1, name: "Carlo"}
 	const explorer2 = {id: 2, name: "Carlo"}
 	const explorer3 = {id: 3, name: "Jorge"}
 	const explorer4 = {id: 4, name: "Luis"}
 	const explorers = [explorer1, explorer2, explorer3,explorer4]
 	res.status(200).json(explorers)
 })
+
+//GET que regresa un explorer por ID
+
+app.get('/v1/explorers/:id', (req,res) => {
+	console.log(`API explorers GET request ${new Date()}`)
+	console.log(`Getting explorer with id ${req.params.id}`)
+	const explorer = {id: 1, name: "Carlo"}
+	res.status(200).json(explorer)
+})
+
 
 app.listen(port, ()=> {
 	console.log(`Example app listening on port ${port}`)
