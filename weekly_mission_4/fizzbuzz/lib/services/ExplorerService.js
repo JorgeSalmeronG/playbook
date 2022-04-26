95380d07ca5450e97f907a2549a012d915e43371
Jorge Salmerón <jsalmeron18@outlook.com>
2022-04-26 14:23:11 -0500

    4) Se agrega archivo ExplorerService con su clase y respectivos métodos

---
+++ weekly_mission_4/fizzbuzz/lib/services/ExplorerService.js
@@ -0,0 +1,22 @@
+class ExplorerService{
+	
+	static filterByMission(explorers,mission){
+		const explorersInMission = explorers.filter( (explorer) => explorer.mission == mission );
+		return explorersInMission
+	}
+
+	static getAmountOfExplorerByMission(explorers,mission){
+		
+		return this.filterByMission(explorers, mission).length
+	}
+
+	static getExplorersUsernameByMission(explorers,mission){
+
+		let explorersInMission = this.filterByMission(explorers, mission)
+		let usernamesInMission = explorersInMission.map( (explorer) => explorer.githubUsername)
+		return usernamesInMission
+
+	}
+
+}
+module.exports = ExplorerService
