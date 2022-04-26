43859888ece0132c2b8d2185017f4d78560d5c46
Jorge Salmerón <jsalmeron18@outlook.com>
2022-04-26 18:55:57 -0500

    8) Se agrega prueba 3 para método 3: Obtener el Github Username

--- weekly_mission_4/fizzbuzz/test/services/ExplorerService.test.js
+++ weekly_mission_4/fizzbuzz/test/services/ExplorerService.test.js
@@ -1,39 +1,38 @@
 const ExplorerService = require('./../../lib/services/ExplorerService')
 
-//const myObj =[{"name" : "explorer1",
-//
-//		"mission" : "node",
-//	 	"score" : 1},
-//		{"name" : "explorer3",
-//		"mission": "java"
-//		"score" : 3},
-//		{ "name" : "explorer5",
-//		"mission": "node",
-//		"score" : 5},
-//		{ "name" : "explorer15"
-//		"mission" : "java",
-//		"score" : 15}
-//		]
 
 describe("Unit tests for ExplorerService class", () =>{
 	
 	test("1) Test if FiterbyMission Method is correct ",() => {
 
 		const explorers = [{ "name" : "explorer1", "mission" : "node"},
-		 		   { "name":"explorer2", "mission" : "java"}]
+		 		{"name":"explorer2", "mission" : "java"},
+				{"name":"explorer3", "mission" : "node"}]
+
 		const result = ExplorerService.filterByMission(explorers,"node")
-		expect(result[0].name).toBe("explorer1")
+		expect(result).toEqual([{ "name" : "explorer1", "mission" : "node"},
+					{"name":"explorer3", "mission" : "node"}])
 	});
 
-	test("2) Test if AmountOfExplorer method is correct ", ()=> {
+	test("2) Test if AmountOfExplorers method is correct ", ()=> {
 
 		const explorers = [{ "name" : "explorer1", "mission" : "node"},
 				{"name":"explorer2", "mission" : "java"},
 				{"name":"explorer3", "mission" : "node"},
 				{"name":"explorer4", "mission" : "node"}]
+
 		const result = ExplorerService.getAmountOfExplorerByMission(explorers, "node")
-		console.log(result)
 		expect(result).toBe(3)
 	});
 
+	test("3) Test if UsernamesByMission method is correct", () => {
+	
+		const explorers = [{ "name" : "explorer1", "mission" : "node", "githubUsername" : 'ajolonauta1'},
+				{"name":"explorer2", "mission" : "java", "githubUsername" : 'ajolonauta2'},
+				{"name":"explorer3", "mission" : "node", "githubUsername" : 'ajolonauta3'},
+				{"name":"explorer4", "mission" : "node", "githubUsername" : 'ajolonauta4'}]
+
+		const result = ExplorerService.getExplorersUsernamesByMission(explorers, "node")
+		expect(result).toEqual(["ajolonauta1","ajolonauta3","ajolonauta4"])
+	})
 })
