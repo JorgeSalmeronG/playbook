4b70af6506f12d04046f390f2aac5fb8d8c3546e
Jorge Salmerón <jsalmeron18@outlook.com>
2022-04-26 18:13:50 -0500

    7) Se agrega prueba para Método2: cantidad de explorers por mission

--- weekly_mission_4/fizzbuzz/test/services/ExplorerService.test.js
+++ weekly_mission_4/fizzbuzz/test/services/ExplorerService.test.js
@@ -1,6 +1,7 @@
 const ExplorerService = require('./../../lib/services/ExplorerService')
 
 //const myObj =[{"name" : "explorer1",
+//
 //		"mission" : "node",
 //	 	"score" : 1},
 //		{"name" : "explorer3",
@@ -19,11 +20,20 @@ describe("Unit tests for ExplorerService class", () =>{
 	test("1) Test if FiterbyMission Method is correct ",() => {
 
 		const explorers = [{ "name" : "explorer1", "mission" : "node"},
-				{"name":"explorer2", "mission" : "java"}]
-							
+		 		   { "name":"explorer2", "mission" : "java"}]
 		const result = ExplorerService.filterByMission(explorers,"node")
-		console.log(result)
 		expect(result[0].name).toBe("explorer1")
-	})
+	});
+
+	test("2) Test if AmountOfExplorer method is correct ", ()=> {
+
+		const explorers = [{ "name" : "explorer1", "mission" : "node"},
+				{"name":"explorer2", "mission" : "java"},
+				{"name":"explorer3", "mission" : "node"},
+				{"name":"explorer4", "mission" : "node"}]
+		const result = ExplorerService.getAmountOfExplorerByMission(explorers, "node")
+		console.log(result)
+		expect(result).toBe(3)
+	});
 
 })
