c827f57e8615184e348e251bee9e8e6b60657bbf
Jorge Salmerón <jsalmeron18@outlook.com>
2022-04-27 16:12:46 -0500

    PARTE 2| Se realizan  cambios menores

--- weekly_mission_4/fizzbuzz/app/app.js
+++ weekly_mission_4/fizzbuzz/app/app.js
@@ -1,6 +1,8 @@
 const Reader = require("./../lib/utils/Reader");
 const ExplorerService = require("./../lib/services/ExplorerService");
 const FizzbuzzService = require("./../lib/services/FizzbuzzService");
+const ExplorerController = require("./../lib/controllers/ExplorerController");
+
 
 const explorers = Reader.readJsonFile("./explorers.json");
 const mission = "node";
@@ -12,3 +14,10 @@ const fizzbuzz = FizzbuzzService.applyValidationInExplorer(explorers,mission);
 console.log(`Explorers: ${qty} \n Mission: ${mission} \n Gitusernames:\n ${gitUsername}`);
 
 fizzbuzz.forEach((item) => console.log(`name: ${item.name}, mission: ${item.mission}, trick: ${item.trick}`));
+
+const data = ExplorerController.getExplorerByMission(mission)
+const user = ExplorerController.getExplorerUsernamesByMission(mission)
+const num = ExplorerController.getExplorerAmountByMission(mission)
+console.log(data)
+console.log(user)
+console.log(num)
