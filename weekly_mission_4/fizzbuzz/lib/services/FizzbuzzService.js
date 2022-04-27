0e15c65cbf7953cd29f997da7232a5b3a855c16e
Jorge Salmerón <jsalmeron18@outlook.com>
2022-04-26 22:36:38 -0500

    Se realizan un par de correcciones

--- weekly_mission_4/fizzbuzz/lib/services/FizzbuzzService.js
+++ weekly_mission_4/fizzbuzz/lib/services/FizzbuzzService.js
@@ -4,14 +4,16 @@ class FizzbuzzService{
 		
 		
 	static applyValidationInExplorer(explorers, mission){
+		
+		const explorersFiltered = ExplorerService.filterByMission(explorers, mission)
 
-		const fizzbuzzExplorer = explorers.map( (explorer) => {
+		const fizzbuzzExplorer = explorersFiltered.map( (explorer) => {
 			if (explorer.score % 3 == 0 && explorer.score % 5 == 0){ explorer.trick = "FIZZBUZZ"}
 			else if (explorer.score % 3 == 0){ explorer.trick = "FIZZ"}
 			else if (explorer.score % 5 == 0){ explorer.trick = "BUZZ"}
 			else explorer.trick = explorer.score
 			})
-		return explorers
+		return explorersFiltered
 	}
 
 }
