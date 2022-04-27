9a3b8d00048ff8bf4f7d8df4e0da412b96828a03
Jorge Salmerón <jsalmeron18@outlook.com>
2022-04-27 18:35:45 -0500

    PARTE 3| 2) Se corrigen inconsistencias de escritura con linter

--- weekly_mission_4/fizzbuzz/lib/controllers/ExplorerController.js
+++ weekly_mission_4/fizzbuzz/lib/controllers/ExplorerController.js
@@ -19,7 +19,7 @@ class ExplorerController {
     }
 
     static getFizzbuzzNumber(number){
-        return FizzbuzzService.applyValidationInNumber(number)
+        return FizzbuzzService.applyValidationInNumber(number);
     }
 }
 module.exports = ExplorerController;
--- weekly_mission_4/fizzbuzz/lib/services/FizzbuzzService.js
+++ weekly_mission_4/fizzbuzz/lib/services/FizzbuzzService.js
@@ -17,10 +17,10 @@ class FizzbuzzService{
     }
 
     static applyValidationInNumber(number){
-            if (number % 3 == 0 && number % 5 == 0){ return "FIZZBUZZ";}
-            else if (number % 3 == 0){ return "FIZZ";}
-            else if (number % 5 == 0){ return "BUZZ";}
-            else return  number
+        if (number % 3 == 0 && number % 5 == 0){ return "FIZZBUZZ";}
+        else if (number % 3 == 0){ return "FIZZ";}
+        else if (number % 5 == 0){ return "BUZZ";}
+        else return  number;
     }
 
 }
