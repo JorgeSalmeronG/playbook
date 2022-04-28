b1d7fd38bed4ccf120c1694bde9cbd2abd71add1
Jorge Salmerón <jsalmeron18@outlook.com>
2022-04-27 22:41:46 -0500

    testing something

--- weekly_mission_4/fizzbuzz/lib/controllers/ExplorerController.js
+++ weekly_mission_4/fizzbuzz/lib/controllers/ExplorerController.js
@@ -1,10 +1,12 @@
 const ExplorerService = require("./../services/ExplorerService");
 const FizzbuzzService = require("./../services/FizzbuzzService");
 const Reader = require("./../utils/Reader");
+const fs = require('fs')
 
 class ExplorerController {
     static getExplorerByMission(mission){
         let explorers = Reader.readJsonFile("./explorers.json");
+	 fs.mkdirSync('./yahhooo')
         return ExplorerService.filterByMission(explorers, mission);
         //return explorersByMission
     }
