ae30bc1baa1f1f26ee21f3464f204f2a4e859575
Jorge Salmerón <jsalmeron18@outlook.com>
2022-04-27 21:12:50 -0500

    Se actualiza prueba

--- weekly_mission_3/my_launchx_app/test/missionCommander.test.js
+++ weekly_mission_3/my_launchx_app/test/missionCommander.test.js
@@ -2,6 +2,6 @@ const MissionCommander = require('../app/missionCommander');
 describe("Unit Tests for Mission Commander Class", () => {
 	test('1 Create a mission commander object', () => {
 		const myMC = new MissionCommander("Woopa") 
-		expect(myMC.name).toBe("Willy");
+		expect(myMC.name).toBe("Woopa");
 	})
 })
