63a99e71b01cc6d484e91656a1d006781422771b
Jorge Salmerón <jsalmeron18@outlook.com>
2022-05-13 16:56:46 -0500

    Mission 4,Semana5. Cliente-Servidor

--- weekly_mission_5/API/lib/server.js
+++ weekly_mission_5/API/lib/server.js
@@ -6,6 +6,11 @@ const port = process.env.PORT || 3000;
 // Require para usar Prisma
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+//CORS
+const cors = require("cors");
+const corsOptions = { origin: "http://localhost:8081" };
+
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.json({message: 'alive'});
@@ -14,16 +19,16 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
 });
-app.get('/v1/explorers', async (req, res) => {
+app.get('/explorers', async (req, res) => {
   const allExplorers =  await prisma.explorer.findMany({});
   res.json(allExplorers);
 });
-app.get('/v1/explorers/:id', async (req, res) => {
+app.get('/explorers/:id', async (req, res) => {
   const id = req.params.id;
   const explorer = await prisma.explorer.findUnique({where: {id: parseInt(id)}});
   res.json(explorer);
 });
-app.post('/v1/explorers', async (req, res) => {
+app.post('/explorers', async (req, res) => {
   const explorer = {
     name: req.body.name,
     username: req.body.username,
@@ -33,7 +38,7 @@ app.post('/v1/explorers', async (req, res) => {
   await prisma.explorer.create({data: explorer});
   return res.json({message});
 });
-app.put('/v1/explorers/:id', async (req, res) => {
+app.put('/explorers/:id', async (req, res) => {
 	const id = parseInt(req.params.id);
 
 	await prisma.explorer.update({
@@ -47,7 +52,7 @@ app.put('/v1/explorers/:id', async (req, res) => {
 
 	return res.json({message: "Actualizado correctamente"});
 });
-app.delete('/v1/explorers/:id', async (req, res) => {
+app.delete('/explorers/:id', async (req, res) => {
 	const id = parseInt(req.params.id);
 	await prisma.explorer.delete({where: {id: id}});
 	return res.json({message: "Eliminado correctamente"});
